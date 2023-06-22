ab79798c7b5fc18278f80506e644d051c67ea699
MariaGlushtsova <glushtsova@tut.by>
2023-06-22 15:40:33 +0300

    Add label and button
    Добавлены элементы label and button

--- IOS10-HW12-Maria Glushtsova/Sources/ViewController.swift
+++ IOS10-HW12-Maria Glushtsova/Sources/ViewController.swift
@@ -10,11 +10,61 @@ import SnapKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Outlets
+    
+    private lazy var timerLabel: UILabel = {
+        let label = UILabel()
+        label.text = "25:00"
+        return label
+    }()
+    
+    private lazy var startButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "custom.play.fill"), for: .normal)
+        return button
+    }()
+    
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupHierarchy()
+        setupLayout()
     }
+    
+    // MARK: - Setup
+    
+    private func setupHierarchy() {
+        
+        view.backgroundColor = UIColor(named: "ViewColor")
+        view.addSubview(timerLabel)
+        view.addSubview(startButton)
+
+    }
+    
+    private func setupLayout() {
+        
+        timerLabel.snp.makeConstraints { make in
+            
+            make.centerX.equalTo(view)
+            make.centerY.equalTo(view)
+            
+        }
+        
+        startButton.snp.makeConstraints { make in
+            
+            make.centerX.equalTo(view)
+            make.centerY.equalTo(timerLabel).offset(150)
+            
+        }
+        
+        }
+    
+    // MARK: - Actions
+    
 
 
+    
 }
 
