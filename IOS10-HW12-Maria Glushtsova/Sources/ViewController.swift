0cecbdbe0b6ee4b2beb8a1f3538fb4e99336008b
MariaGlushtsova <glushtsova@tut.by>
2023-08-02 13:19:55 +0300

    Add progress bar

--- IOS10-HW12-Maria Glushtsova/Sources/ViewController.swift
+++ IOS10-HW12-Maria Glushtsova/Sources/ViewController.swift
@@ -33,27 +33,45 @@ class ViewController: UIViewController {
         button.setTitle("", for: .normal)
         button.setImage(UIImage(systemName: "play.fill"), for: .normal)
         button.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
-//        button.setImage(UIImage(systemName: "pause.fill"), for: .selected)
         button.tintColor = UIColor(named: "buttonColor")
         return button
     }()
+    
+    private lazy var shapeView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "circle")
+        return imageView
+    }()
+    
+    let shapeLayer = CAShapeLayer()
+    
+
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.circleAnimation()
+        
+    }
     
     
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         setupHierarchy()
         setupLayout()
+        
     }
     
     // MARK: - Setup
     
     private func setupHierarchy() {
         
-        view.backgroundColor = UIColor(named: "ViewColor")
+        view.backgroundColor = UIColor(named: "viewColor")
         view.addSubview(timerLabel)
         view.addSubview(startButton)
+        view.addSubview(shapeView)
         
     }
     
@@ -69,7 +87,16 @@ class ViewController: UIViewController {
         startButton.snp.makeConstraints { make in
             
             make.centerX.equalTo(view)
-            make.centerY.equalTo(timerLabel).offset(150)
+            make.centerY.equalTo(timerLabel).offset(180)
+            
+        }
+        
+        shapeView.snp.makeConstraints { make in
+            
+            make.width.equalTo(600)
+            make.height.equalTo(600)
+            make.centerX.equalTo(timerLabel)
+            make.centerY.equalTo(timerLabel).offset(-50)
             
         }
         
@@ -90,12 +117,13 @@ class ViewController: UIViewController {
         
         sender.isSelected.toggle();
         if startButton.isSelected {
-            
+            clockwiseAnimation()
+            resumeAnimation()
             startButton.setImage(UIImage(systemName: "pause.fill"), for: .selected)
             timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
 
         } else {
-            
+            pauseAnimation()
             startButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
             timer.invalidate()
         }
@@ -116,6 +144,7 @@ class ViewController: UIViewController {
                 
                 timeToShow = workTimeRemaining
                 workTimeRemaining -= 1
+                
                 
             } else {
                 
@@ -140,7 +169,52 @@ class ViewController: UIViewController {
             
         }
         
+    }
+    
+    // MARK: - Animation
+    
+    func circleAnimation() {
+        
+        let endAngle = (-CGFloat.pi / 2)
+        let startAngle = 2 * CGFloat.pi + endAngle
+        let center = CGPoint(x: shapeView.frame.width / 2, y: shapeView.frame.height / 2)
+        let circulePath = UIBezierPath(arcCenter: center, radius: 138, startAngle: startAngle, endAngle: endAngle, clockwise: false)
+        
+        shapeLayer.path = circulePath.cgPath
+        shapeLayer.lineWidth = 40
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeEnd = 1
+        shapeLayer.lineCap = CAShapeLayerLineCap.round
+        shapeLayer.strokeColor = UIColor(named: "buttonColor")?.cgColor
+        shapeView.layer.addSublayer(shapeLayer)
         
     }
     
+     func clockwiseAnimation() {
+        
+        let clockwiseAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        clockwiseAnimation.toValue = 0
+        clockwiseAnimation.duration = CFTimeInterval(timeToShow)
+        clockwiseAnimation.fillMode = CAMediaTimingFillMode.forwards
+        clockwiseAnimation.isRemovedOnCompletion = false
+        shapeLayer.add(clockwiseAnimation, forKey: "clockwiseAnimation")
+        
+    }
+    
+    func pauseAnimation() {
+       
+        let pausedTime = shapeLayer.convertTime(CACurrentMediaTime(), to: nil)
+        shapeLayer.speed = 0.0
+        shapeLayer.timeOffset = pausedTime
+
+   }
+    
+    func resumeAnimation() {
+
+        let resumeTime = shapeLayer.convertTime(CACurrentMediaTime(), to: nil)
+        shapeLayer.speed = 1
+        shapeLayer.timeOffset = 0
+        shapeLayer.beginTime = resumeTime
+        
+    }
 }
